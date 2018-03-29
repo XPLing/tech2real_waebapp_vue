f5742d9e757d3957c5c5573d088c8461e041edf9
XPL <616954589@qq.com>
2018-03-29 18:10:59 +0800

    vee-validate 原本不可见元素到可见后的验证

--- src/components/user/register/register.vue
+++ src/components/user/register/register.vue
@@ -3,29 +3,29 @@
     <form class="form" data-vv-scope="registerFirst">
       <input type="hidden" id="userGuid">
       <p class="verify error"></p>
-      <ul class="form-main form-hook step-item user-hook active">
+      <ul class="form-main form-hook step-item user-hook active" v-if="!passsFirst">
         <li class="input-item" :class="{'has-error': errors.has('registerFirst.nickname')}">
           <div class="main">
             <i class="icon">昵&nbsp;&nbsp;&nbsp;&nbsp;称</i>
             <input type="text" placeholder="请输入昵称" name="nickname"
-                   v-validate="'required|nickname'"
+                   v-validate="'required|nickname'" v-model="nickname"
                    :class="{'input': true, 'has-error': errors.has('registerFirst.nickname') }">
-            <i class="fa fa-exclamation-circle tipinfo"></i>
+            <!--<i class="fa fa-exclamation-circle tipinfo"></i>-->
           </div>
           <div v-show="errors.has('registerFirst.nickname')" class="c-tip error">
-            <i class="icon fa fa-warning text-danger"></i>
+            <i class="icon fa fa-exclamation-circle text-danger"></i>
             <span class="meg text-danger">{{ errors.first('registerFirst.nickname')}}</span>
           </div>
         </li>
         <li class="input-item" :class="{'has-error': errors.has('registerFirst.phone') }">
           <div class="main">
             <i class="icon">手机号</i>
-            <input name="phone" v-validate="'required|phone'"
+            <input name="phone" v-validate="'required|phone'" v-model="phone"
                    :class="{'input': true, 'has-error': errors.has('registerFirst.phone') }" type="text"
                    placeholder="请输入手机号">
           </div>
           <div v-show="errors.has('registerFirst.phone')" class="c-tip error">
-            <i class="icon fa fa-warning text-danger"></i>
+            <i class="icon fa fa-exclamation-circle text-danger"></i>
             <span class="meg text-danger">{{ errors.first('registerFirst.phone')}}</span>
           </div>
         </li>
@@ -37,7 +37,7 @@
                    :class="{'input': true, 'has-error': errors.has('registerFirst.password')}">
           </div>
           <div v-show="errors.has('registerFirst.password')" class="c-tip error">
-            <i class="icon fa fa-warning text-danger"></i>
+            <i class="icon fa fa-exclamation-circle text-danger"></i>
             <span class="meg text-danger">{{ errors.first('registerFirst.password')}}</span>
           </div>
         </li>
@@ -47,26 +47,26 @@
             <input type="password" placeholder="确认密码" name="confirmPW"
                    v-validate="'required|confirmed:password'"
                    :class="{'input': true, 'has-error': errors.has('registerFirst.confirmPW')}">
-            <i class="fa fa-exclamation-circle tipinfo"></i>
+            <!--<i class="fa fa-exclamation-circle tipinfo"></i>-->
           </div>
           <div v-show="errors.has('registerFirst.confirmPW')" class="c-tip error">
-            <i class="icon fa fa-warning text-danger"></i>
+            <i class="icon fa fa-exclamation-circle text-danger"></i>
             <span class="meg text-danger">{{ errors.first('registerFirst.confirmPW')}}</span>
           </div>
         </li>
       </ul>
-      <ul class="form-main form-hook step-item verifycode-hook">
+      <ul class="form-main form-hook step-item verifycode-hook" v-if="passsFirst">
         <li class="input-item verify-code" :class="{'has-error': errors.has('registerFirst.verifycode') }">
           <div class="main">
             <i class="icon">验证码</i>
-            <input class="input" v-validate="'required'" type="text" placeholder="请输入验证码" name="verifycode"
-                   :class="{'input': true}">
+            <input class="input" type="text" placeholder="请输入验证码" name="verifycode" key="verifycode"
+                   v-validate="'required'" v-model="verifycode" :class="{'input': true}">
             <span class="tipinfo send-hook">
                 <em>发送信息</em>
             </span>
           </div>
           <div v-show="errors.has('registerFirst.verifycode')" class="c-tip error">
-            <i class="icon fa fa-warning text-danger"></i><span
+            <i class="icon fa fa-exclamation-circle text-danger"></i><span
             class="meg text-danger">{{ errors.first('registerFirst.verifycode')
             }}</span>
           </div>
@@ -89,7 +89,6 @@
   import { commonVariable, ERR_OK } from 'api/config';
   import { Validator } from 'vee-validate';
 
-
   //  const dictionary = {
   //    zh_CN: {
   //      messages: {
@@ -102,7 +101,10 @@
     data () {
       return {
         password: '',
-        phone: ''
+        verifycode: '',
+        nickname: '',
+        phone: '',
+        passsFirst: false
       };
     },
     mounted () {
@@ -111,7 +113,10 @@
       validateForm (scope) {
         this.$validator.validateAll(scope).then((res) => {
           console.log(res);
-          console.log('Form Submitted!');
+          if (res) {
+            this.passsFirst = true;
+            console.log('Form Submitted!');
+          }
         }, (erro) => {
           console.log('Form erro!');
         });
